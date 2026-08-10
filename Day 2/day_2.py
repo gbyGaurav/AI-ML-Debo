c0de35f9143e18d138e1941996c1646715636dda
Gaurav <bharatdeore7813@gmail.com>
2026-08-10 21:04:20 +0530

    Add Day 2 Python fundamentals

---
+++ Day 2/day_2.py
@@ -0,0 +1,116 @@
+
+''' Day 2: Python Fundamentals
+   Name: Gaurav Bharat Deore
+   Contact no : 9373446328
+   Email :bharatdeore7813@gmail.com
+   Description : Topics that are covered on day 2
+   Topics : 1) Arithmatic Operators,2)String Concatination ,3) String Replication ,4) variable naming rule , 5) comments, 6) print function , 
+   7) input function, 8) len function , 9)type conversion, 10) round() function, 11)  abs() function 12)comparison (relational) operators ,
+  13)Boolean Operator  , 14) Minor project'''
+
+# Arithmatic Operations
+
+x = 5
+y = 4
+
+# Exponent
+x ** 3
+
+# Modulus
+x % y
+
+# Integer Division
+x // y
+
+# Divsion
+x / y
+
+# multiplication
+x *y
+
+# Addition
+x + y
+
+# Substraction
+x - y
+
+# String Concatination
+
+x= 'my name is'
+y = 'gaurav'
+z = " "
+print(x+z+y)
+
+# String Replication
+
+x= 'gaurav'
+print(x * 3)
+
+# variable assignment
+
+var1 = 3.14
+print(var1)
+
+var1 = 'abc'
+print(var1)
+
+# variable naming rule
+
+# comments
+
+# print function
+
+# input function
+
+# len function
+
+# type conversion int() , str(), float()
+
+# int()
+x = 22.5
+
+print('My age is ',int(x))
+
+#str()
+x = 22.5
+
+print('My age is ',str(x))
+
+# float
+x = 22
+
+print('My age is ',float(x))
+
+# round function
+
+x =78.5
+print(round(x))
+
+# abs() function (absolute) - converts negative values to positive
+
+x= -7.49
+print(abs(x))
+
+# comparison (relational) operators
+
+x = int(input("Enter no:"))
+
+if x == 5:
+  print("Number matched!")
+elif x != 5:
+  print("Not matched!")
+  if x >= 5:
+    print("Number is Greater than 5")
+    if x > 10 :
+      print("Number is Greater than 10")
+  elif x <= 5:
+    print("Number is Smaller than 5")
+    if x < 10:
+      print("Number is Smaller than 10")
+
+# Boolean Operator
+
+# Truth table
+
+
+
